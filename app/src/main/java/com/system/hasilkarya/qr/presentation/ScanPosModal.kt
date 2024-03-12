9519d4835895d26a4bc6e9a3c119bd92b31c9c80
Sendiko <sarangtawon897@gmail.com>
2024-03-12 20:36:21 +0700

    enc(add): QR Scanner Modals

---
+++ app/src/main/java/com/system/hasilkarya/qr/presentation/ScanPosModal.kt
@@ -0,0 +1,182 @@
+package com.system.hasilkarya.qr.presentation
+
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
+import com.system.hasilkarya.qr.domain.BarcodeAnalyzer
+import kotlinx.coroutines.launch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScanPosModal(
+    sheetState: SheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    ),
+    onResult: (String) -> Unit
+) {
+    val coroutineScope = rememberCoroutineScope()
+    var result by remember {
+        mutableStateOf("")
+    }
+    LaunchedEffect(key1 = null, block = {sheetState.expand()})
+    ModalBottomSheet(
+        modifier = Modifier.fillMaxSize(),
+        sheetState = sheetState,
+        shape = RectangleShape,
+        onDismissRequest = { coroutineScope.launch { sheetState.expand() } },
+        content = {
+            val context = LocalContext.current
+            val lifecycleOwner = LocalLifecycleOwner.current
+            var preview by remember { mutableStateOf<Preview?>(null) }
+
+            Column(
+                Modifier
+                    .background(MaterialTheme.colorScheme.background)
+                    .fillMaxSize()
+            ){
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    AndroidView(
+                        factory = { androidViewContext ->
+                            PreviewView(androidViewContext).apply {
+                                this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                                layoutParams = ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.MATCH_PARENT,
+                                    ViewGroup.LayoutParams.MATCH_PARENT,
+                                )
+                                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                            }
+                        },
+                        modifier = Modifier.aspectRatio(1f),
+                        update = { previewView ->
+                            val cameraSelector: CameraSelector = CameraSelector.Builder()
+                                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                .build()
+                            val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+                            val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                                ProcessCameraProvider.getInstance(context)
+
+                            cameraProviderFuture.addListener({
+                                preview = Preview.Builder().build().also {
+                                    it.setSurfaceProvider(previewView.surfaceProvider)
+                                }
+                                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                                val barcodeAnalyser = BarcodeAnalyzer { barcodes ->
+                                    barcodes.forEach { barcode ->
+                                        barcode.rawValue?.let { barcodeValue ->
+                                            result = barcodeValue
+                                        }
+                                    }
+                                }
+                                val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                    .build()
+                                    .also {
+                                        it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                                    }
+
+                                try {
+                                    cameraProvider.unbindAll()
+                                    cameraProvider.bindToLifecycle(
+                                        lifecycleOwner,
+                                        cameraSelector,
+                                        preview,
+                                        imageAnalysis
+                                    )
+                                } catch (e: Exception) {
+                                    Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
+                                }
+                            }, ContextCompat.getMainExecutor(context))
+                        }
+                    )
+                    Box(
+                        modifier = Modifier
+                            .background(Color.Transparent)
+                            .border(width = 3.dp, color = MaterialTheme.colorScheme.onPrimary)
+                            .size(200.dp),
+                    )
+                }
+                Column(
+                    modifier = Modifier.background(MaterialTheme.colorScheme.background),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(
+                                vertical = 24.dp,
+                                horizontal = 24.dp
+                            )
+                            .fillMaxWidth(),
+                        text = if (result.isNotBlank()) "Scan QR berhasil!" else "Scan QR Pos!",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                    )
+                    Spacer(modifier = Modifier.size(16.dp))
+                    AnimatedVisibility(
+                        visible = result.isNotBlank(),
+                        enter = fadeIn(),
+                        exit = fadeOut(),
+                        content = {
+                            Button(
+                                onClick = { onResult(result) },
+                                content = {
+                                    Text(text = "Lanjut simpen data")
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    )
+}
