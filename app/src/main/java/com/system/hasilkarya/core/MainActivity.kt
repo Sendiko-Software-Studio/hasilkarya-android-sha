8a83d4a8de87dc98e935b31bf5bee085f3c7c16c
Sendiko <sarangtawon897@gmail.com>
2024-03-12 12:47:03 +0700

    enc(add): LoginScreen to navigation

--- app/src/main/java/com/system/hasilkarya/core/MainActivity.kt
+++ app/src/main/java/com/system/hasilkarya/core/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.system.hasilkarya.core.navigation.Destination
 import com.system.hasilkarya.core.ui.theme.HasilKaryaTheme
+import com.system.hasilkarya.login.presentation.LoginScreen
+import com.system.hasilkarya.login.presentation.LoginScreenViewModel
 import com.system.hasilkarya.splash.presentation.SplashScreen
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +29,27 @@ class MainActivity : ComponentActivity() {
                         composable(
                             route = Destination.SplashScreen.name,
                             content = {
-                                SplashScreen()
+                                SplashScreen(
+                                    onNavigate = { route ->
+                                        navController.navigate(
+                                            route = route
+                                        ) {
+                                            popUpTo(
+                                                route,
+                                            ) { inclusive = true }
+                                        }
+                                    }
+                                )
+                            }
+                        )
+                        composable(
+                            route = Destination.LoginScreen.name,
+                            content = {
+                                val viewModel: LoginScreenViewModel = viewModel()
+                                LoginScreen(
+                                    state = viewModel.state.collectAsState().value,
+                                    onEvent = viewModel::onEvent
+                                )
                             }
                         )
                     }
