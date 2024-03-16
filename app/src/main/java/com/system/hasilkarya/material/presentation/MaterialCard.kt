0cce38f42aff1f96a3f6228e894c9f1d88616647
Sendiko <sarangtawon897@gmail.com>
2024-03-17 03:59:04 +0700

    enc(add): improve material component

--- app/src/main/java/com/system/hasilkarya/material/presentation/MaterialCard.kt
+++ app/src/main/java/com/system/hasilkarya/material/presentation/MaterialCard.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.QrCodeScanner
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,10 +16,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.system.hasilkarya.R
+import com.system.hasilkarya.core.ui.theme.poppinsFont
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,7 +35,6 @@ fun MaterialCard(
         onClick = { onClickAction() },
         content = {
             Box(
-                modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center,
                 content = {
                     Column(
@@ -44,16 +43,17 @@ fun MaterialCard(
                         modifier = Modifier.padding(24.dp)
                     ) {
                         Icon(
-                            imageVector = Icons.Default.QrCodeScanner,
-                            contentDescription = "scan qr",
-                            modifier = Modifier.size(128.dp)
+                            painter = painterResource(id = R.drawable.scanner),
+                            contentDescription = "scan",
+                            modifier = Modifier.size(86.dp)
                         )
                         Spacer(modifier = Modifier.height(8.dp))
                         Text(
+                            modifier = Modifier.width(128.dp),
                             text = "Scan Material Movement",
                             fontSize = 16.sp,
-                            modifier = Modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Center
+                            textAlign = TextAlign.Center,
+                            fontFamily = poppinsFont
                         )
                     }
                 }
---
+++ app/src/main/java/com/system/hasilkarya/material/presentation/MaterialListItem.kt
@@ -0,0 +1,91 @@
+package com.system.hasilkarya.material.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.system.hasilkarya.core.ui.theme.poppinsFont
+import com.system.hasilkarya.dashboard.data.MaterialEntity
+
+@Composable
+fun MaterialListItem(
+    modifier: Modifier = Modifier,
+    materialEntity: MaterialEntity
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(space = 8.dp, alignment = Alignment.Top),
+        horizontalAlignment = Alignment.End,
+    ) {
+        AssistChip(onClick = {  }, label = { Text(text = "Menunggu jaringan") })
+        Text(
+            text = "driverId: ${materialEntity.driverId.take(8)}...",
+            fontFamily = poppinsFont,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+        Text(
+            text = "truckId: ${materialEntity.truckId.take(8)}...",
+            fontFamily = poppinsFont,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+        Text(
+            text = "stationId: ${materialEntity.stationId.take(8)}...",
+            fontFamily = poppinsFont,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+        Text(
+            text = "checkerId: ${materialEntity.checkerId.take(8)}...",
+            fontFamily = poppinsFont,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+        Text(
+            text = "rasio observasi: ${materialEntity.ratio}",
+            fontFamily = poppinsFont,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+        Text(
+            text = "keterangan: ${materialEntity.remarks}...",
+            fontFamily = poppinsFont,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+    }
+    Divider(modifier = Modifier.fillMaxWidth())
+}
+
+@Preview
+@Composable
+fun MaterialListPrev() {
+    Surface {
+        val data = MaterialEntity(
+            checkerId = "2308b833-4bd3-4dcf-8813-6df57e5c22d6",
+            driverId = "c12639e5-ccaa-4cab-82e8-6d768d0db174",
+            ratio = 0.6,
+            remarks = "sesuai test gagal",
+            stationId = "854c43db-d477-4329-9c2c-3e24d700a2d6",
+            truckId = "f0b6efac-0ee6-4995-8532-a6c32402f402"
+        )
+        Column {
+            MaterialListItem(materialEntity = data, modifier = Modifier.padding(16.dp))
+            MaterialListItem(materialEntity = data, modifier = Modifier.padding(16.dp))
+            MaterialListItem(materialEntity = data, modifier = Modifier.padding(16.dp))
+            MaterialListItem(materialEntity = data, modifier = Modifier.padding(16.dp))
+
+        }
+    }
+}
