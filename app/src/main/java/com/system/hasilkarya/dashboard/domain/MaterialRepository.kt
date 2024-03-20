cccbfda39cd65a2cfd6f2b028a79f691d52b4e0c
Sendiko <sarangtawon897@gmail.com>
2024-03-21 05:07:12 +0700

    enc(add): post to log method

--- app/src/main/java/com/system/hasilkarya/dashboard/domain/MaterialRepository.kt
+++ app/src/main/java/com/system/hasilkarya/dashboard/domain/MaterialRepository.kt
@@ -3,6 +3,7 @@ package com.system.hasilkarya.dashboard.domain
 import com.system.hasilkarya.core.network.ApiServices
 import com.system.hasilkarya.dashboard.data.MaterialEntity
 import com.system.hasilkarya.dashboard.data.PostMaterialRequest
+import com.system.hasilkarya.dashboard.data.PostToLogRequest
 import javax.inject.Inject
 
 class MaterialRepository @Inject constructor(
@@ -13,6 +14,7 @@ class MaterialRepository @Inject constructor(
     suspend fun saveMaterial(material: MaterialEntity) = dao.saveMaterial(material)
     suspend fun deleteMaterial(material: MaterialEntity) = dao.deleteMaterial(material)
     fun postMaterial(token: String, material: PostMaterialRequest) = apiServices.postMaterial(token, material)
+    fun postToLog(token: String, material: PostToLogRequest) = apiServices.postToLog(token, material)
     fun getMaterials() = dao.getAllMaterial()
     fun checkDriverId(token: String, driverId: String) = apiServices.checkDriverId(driverId, token)
     fun checkTruckId(token: String, truckId: String) = apiServices.checkTruckId(truckId, token)
