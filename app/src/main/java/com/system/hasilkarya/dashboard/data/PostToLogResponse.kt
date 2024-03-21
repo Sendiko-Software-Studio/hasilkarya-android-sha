82fb61d46f2c346b437968813792f8ab0c872a8e
Sendiko <sarangtawon897@gmail.com>
2024-03-21 16:10:27 +0700

    enc(add): add post to log response, rename data

---
+++ app/src/main/java/com/system/hasilkarya/dashboard/data/PostToLogResponse.kt
@@ -0,0 +1,54 @@
+package com.system.hasilkarya.dashboard.data
+
+import com.google.gson.annotations.SerializedName
+
+data class PostToLogResponse(
+
+	@field:SerializedName("data")
+	val data: Data? = null,
+
+	@field:SerializedName("success")
+	val success: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+data class Data(
+
+	@field:SerializedName("date")
+	val date: Any? = null,
+
+	@field:SerializedName("truck_id")
+	val truckId: String? = null,
+
+	@field:SerializedName("driver_id")
+	val driverId: String? = null,
+
+	@field:SerializedName("code")
+	val code: Any? = null,
+
+	@field:SerializedName("error_log")
+	val errorLog: String? = null,
+
+	@field:SerializedName("observation_ratio_percentage")
+	val observationRatioPercentage: Any? = null,
+
+	@field:SerializedName("station_id")
+	val stationId: String? = null,
+
+	@field:SerializedName("solid_ratio")
+	val solidRatio: Any? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("checker_id")
+	val checkerId: String? = null,
+
+	@field:SerializedName("truck_capacity")
+	val truckCapacity: Any? = null,
+
+	@field:SerializedName("remarks")
+	val remarks: Any? = null
+)
