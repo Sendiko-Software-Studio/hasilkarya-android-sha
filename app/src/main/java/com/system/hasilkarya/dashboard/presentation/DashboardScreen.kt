0b9cdfefc9a650ae1a62eb2b51db85d7c8c139a4
Sendiko <sarangtawon897@gmail.com>
2024-03-26 18:54:30 +0700

    enc(add): lil adjustment

--- app/src/main/java/com/system/hasilkarya/dashboard/presentation/DashboardScreen.kt
+++ app/src/main/java/com/system/hasilkarya/dashboard/presentation/DashboardScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
@@ -141,11 +143,11 @@ fun DashboardScreen(
                     }
                 }
                 item {
+                    Spacer(modifier = Modifier.height(16.dp))
                     AnimatedVisibility(
                         visible = state.materials.isNotEmpty(),
                         enter = expandHorizontally(),
                         exit = shrinkHorizontally()
-
                     ) {
                         UnsentItemCard(
                             modifier = Modifier
@@ -155,11 +157,11 @@ fun DashboardScreen(
                     }
                 }
                 item {
+                    Spacer(modifier = Modifier.height(16.dp))
                     AnimatedVisibility(
                         visible = state.fuels.isNotEmpty(),
                         enter = expandHorizontally(),
                         exit = shrinkHorizontally()
-
                     ) {
                         UnsentItemCard(
                             modifier = Modifier
