104f4a70d5f97d7204d115a9995d9bbbfb049fbd
Sendiko <sarangtawon897@gmail.com>
2024-03-29 19:04:20 +0700

    enc(add): dao

--- app/src/main/java/com/system/hasilkarya/core/repositories/AppDatabase.kt
+++ app/src/main/java/com/system/hasilkarya/core/repositories/AppDatabase.kt
@@ -5,19 +5,22 @@ import androidx.room.Database
 import androidx.room.DeleteTable
 import androidx.room.RoomDatabase
 import androidx.room.migration.AutoMigrationSpec
+import com.system.hasilkarya.core.entities.FuelHeavyVehicleEntity
 import com.system.hasilkarya.core.entities.FuelTruckEntity
 import com.system.hasilkarya.core.entities.MaterialEntity
+import com.system.hasilkarya.core.repositories.fuel.heavy_vehicle.HeavyVehicleDao
 import com.system.hasilkarya.core.repositories.fuel.truck.TruckFuelDao
 import com.system.hasilkarya.core.repositories.material.MaterialDao
 
 @Database(
-    entities = [MaterialEntity::class, FuelTruckEntity::class],
-    version = 3,
-    autoMigrations = [AutoMigration(from = 2, to = 3, Migration23::class)]
+    entities = [MaterialEntity::class, FuelTruckEntity::class, FuelHeavyVehicleEntity::class],
+    version = 4,
+    autoMigrations = [AutoMigration(from = 3, to = 4)]
 )
 abstract class AppDatabase: RoomDatabase() {
     abstract val materialDao: MaterialDao
     abstract val truckFuelDao: TruckFuelDao
+    abstract val heavyVehicleDao: HeavyVehicleDao
 }
 
 @DeleteTable.Entries(
