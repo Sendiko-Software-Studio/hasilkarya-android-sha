b718cce5a24c6e9db6c553c8a5eab3330a420d94
Sendiko <sarangtawon897@gmail.com>
2024-03-29 19:43:28 +0700

    enc(add): scan option

--- app/src/main/java/com/system/hasilkarya/dashboard/presentation/component/ScanOptions.kt
+++ app/src/main/java/com/system/hasilkarya/dashboard/presentation/component/ScanOptions.kt
@@ -1,5 +1,5 @@
 package com.system.hasilkarya.dashboard.presentation.component
 
 enum class ScanOptions {
-    Driver, Truck, Pos, Volume, None
+    Driver, Truck, Pos, Volume, HeavyVehicle, None
 }
