7505d3d15531f7fa3278fb8d5462194cf879b2e9
Sendiko <sarangtawon897@gmail.com>
2024-03-30 21:06:26 +0700

    enc(add): req, res

---
+++ app/src/main/java/com/system/hasilkarya/heavy_vehicle_fuel/data/HeavyVehicleLogRequest.kt
@@ -0,0 +1,30 @@
+package com.system.hasilkarya.heavy_vehicle_fuel.data
+
+import com.google.gson.annotations.SerializedName
+
+data class HeavyVehicleLogRequest(
+
+	@field:SerializedName("volume")
+	val volume: Any,
+
+	@field:SerializedName("driver_id")
+	val driverId: String,
+
+	@field:SerializedName("error_log")
+	val errorLog: String,
+
+	@field:SerializedName("hourmeter")
+	val hourmeter: Any,
+
+	@field:SerializedName("station_id")
+	val stationId: String,
+
+	@field:SerializedName("gas_operator_id")
+	val gasOperatorId: String,
+
+	@field:SerializedName("heavy_vehicle_id")
+	val heavyVehicleId: String,
+
+	@field:SerializedName("remarks")
+	val remarks: String
+)
