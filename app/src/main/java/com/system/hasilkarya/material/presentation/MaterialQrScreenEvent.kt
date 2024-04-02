995f86c8006e13eb7bc7feb43e37fc9def42f46e
Sendiko <sarangtawon897@gmail.com>
2024-04-02 10:43:16 +0700

    enc(add): fix connection bugs

--- app/src/main/java/com/system/hasilkarya/material/presentation/MaterialQrScreenEvent.kt
+++ app/src/main/java/com/system/hasilkarya/material/presentation/MaterialQrScreenEvent.kt
@@ -1,15 +1,16 @@
 package com.system.hasilkarya.material.presentation
 
+import com.system.hasilkarya.core.network.Status
 import com.system.hasilkarya.dashboard.presentation.component.ScanOptions
 
 sealed class MaterialQrScreenEvent {
-    data class OnDriverIdRegistered(val driverId: String): MaterialQrScreenEvent()
-    data class OnTruckIdRegistered(val truckId: String): MaterialQrScreenEvent()
-    data class OnPosIdRegistered(val posId: String): MaterialQrScreenEvent()
-    data class OnSelectedRatio(val ratio: Double): MaterialQrScreenEvent()
+    data class OnDriverIdRegistered(val driverId: String, val connectionStatus: Status): MaterialQrScreenEvent()
+    data class OnTruckIdRegistered(val truckId: String, val connectionStatus: Status): MaterialQrScreenEvent()
+    data class OnPosIdRegistered(val posId: String, val connectionStatus: Status): MaterialQrScreenEvent()
+    data class OnVolumeChange(val volume: String): MaterialQrScreenEvent()
     data class OnNewRemarks(val remarks: String): MaterialQrScreenEvent()
     data class OnClearRemarks(val remarks: String = ""): MaterialQrScreenEvent()
     data class OnNavigateForm(val scanOptions: ScanOptions): MaterialQrScreenEvent()
     data object OnClearNotification: MaterialQrScreenEvent()
-    data object SaveMaterial: MaterialQrScreenEvent()
+    data class SaveMaterial(val connectionStatus: Status): MaterialQrScreenEvent()
 }
