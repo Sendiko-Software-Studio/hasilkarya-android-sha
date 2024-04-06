ff39792f78009dc4e4ad39f8ae38a509cc12d567
Sendiko <sarangtawon897@gmail.com>
2024-04-06 19:21:26 +0700

    gradle(): upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.system.hasilkarya"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.22r"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
