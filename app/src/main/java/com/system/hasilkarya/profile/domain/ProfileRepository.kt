982b968500ffe9306f5b804982148c9fea289a1d
Sendiko <sarangtawon897@gmail.com>
2024-04-22 22:02:38 +0700

    enc(add): get and set theme method

--- app/src/main/java/com/system/hasilkarya/profile/domain/ProfileRepository.kt
+++ app/src/main/java/com/system/hasilkarya/profile/domain/ProfileRepository.kt
@@ -2,6 +2,7 @@ package com.system.hasilkarya.profile.domain
 
 import com.system.hasilkarya.core.network.ApiServices
 import com.system.hasilkarya.core.preferences.AppPreferences
+import com.system.hasilkarya.core.ui.theme.AppTheme
 import javax.inject.Inject
 
 class ProfileRepository @Inject constructor(
@@ -12,6 +13,8 @@ class ProfileRepository @Inject constructor(
     fun getName() = preferences.getName()
     fun getToken() = preferences.getToken()
     fun getEmail() = preferences.getEmail()
+    fun getTheme() = preferences.getTheme()
+    suspend fun setTheme(theme: AppTheme) = preferences.setTheme(theme)
     suspend fun clearData() = preferences.clearData()
     fun logout(token: String) = apiServices.logout(token)
 }
