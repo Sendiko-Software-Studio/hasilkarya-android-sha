8e1fd2b24a41fb90515fc209f5d0531126345bb7
Sendiko <sarangtawon897@gmail.com>
2024-05-17 17:05:13 +0700

    enc(add): add station entity

--- app/src/main/java/com/system/hasilkarya/core/repositories/AppDatabase.kt
+++ app/src/main/java/com/system/hasilkarya/core/repositories/AppDatabase.kt
@@ -8,22 +8,30 @@ import androidx.room.migration.AutoMigrationSpec
 import com.system.hasilkarya.core.entities.FuelHeavyVehicleEntity
 import com.system.hasilkarya.core.entities.FuelTruckEntity
 import com.system.hasilkarya.core.entities.MaterialEntity
+import com.system.hasilkarya.core.entities.StationEntity
 import com.system.hasilkarya.core.repositories.fuel.heavy_vehicle.HeavyVehicleDao
 import com.system.hasilkarya.core.repositories.fuel.truck.TruckFuelDao
 import com.system.hasilkarya.core.repositories.material.MaterialDao
+import com.system.hasilkarya.core.repositories.station.StationDao
 
 @Database(
-    entities = [MaterialEntity::class, FuelTruckEntity::class, FuelHeavyVehicleEntity::class],
-    version = 6,
-    autoMigrations = [AutoMigration(from = 5, to = 6)]
+    entities = [
+        MaterialEntity::class,
+        FuelTruckEntity::class,
+        FuelHeavyVehicleEntity::class,
+        StationEntity::class
+    ],
+    version = 7,
+    autoMigrations = [AutoMigration(from = 6, to = 7)]
 )
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract val materialDao: MaterialDao
     abstract val truckFuelDao: TruckFuelDao
     abstract val heavyVehicleDao: HeavyVehicleDao
+    abstract val stationDao: StationDao
 }
 
 @DeleteTable.Entries(
     DeleteTable(tableName = "gas")
 )
-class Migration23: AutoMigrationSpec
+class Migration23 : AutoMigrationSpec
