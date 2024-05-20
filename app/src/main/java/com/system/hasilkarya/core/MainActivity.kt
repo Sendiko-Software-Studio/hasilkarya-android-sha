e12e8f022952850f6a3fedd5b5f90fd4a73ad2fc
Sendiko <sarangtawon897@gmail.com>
2024-05-21 00:01:22 +0700

    enc(del): connection status

--- app/src/main/java/com/system/hasilkarya/core/MainActivity.kt
+++ app/src/main/java/com/system/hasilkarya/core/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.system.hasilkarya.core
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,6 +38,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -113,7 +116,6 @@ class MainActivity : ComponentActivity() {
                                 MaterialQrScreen(
                                     state = viewModel.state.collectAsState().value,
                                     onEvent = viewModel::onEvent,
-                                    connectionStatus = viewModel.connectionStatus.collectAsState().value.connectionStatus,
                                     onNavigateBack = { route ->
                                         navController.navigate(
                                             route = route.name
