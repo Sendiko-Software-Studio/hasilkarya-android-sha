51bbe66aa474ddbed5616ec01ed3972cbcccf136
Sendiko <sarangtawon897@gmail.com>
2024-05-22 21:17:33 +0700

    enc(add): delete unused method

--- app/src/main/java/com/system/hasilkarya/core/repositories/material/MaterialRepository.kt
+++ app/src/main/java/com/system/hasilkarya/core/repositories/material/MaterialRepository.kt
@@ -18,10 +18,7 @@ class MaterialRepository @Inject constructor(
     fun postMaterial(token: String, material: PostMaterialRequest) = apiServices.postMaterial(token, material)
     fun postToLog(token: String, material: PostMaterialLogRequest) = apiServices.postToLog(token, material)
     fun getMaterials() = dao.getAllMaterial()
-    fun checkDriverId(token: String, driverId: String) = apiServices.checkDriverId(driverId, token)
+    fun (token: String, driverId: String) = apiServices.checkDriverId(driverId, token)
     fun checkTruckId(token: String, truckId: String) = apiServices.checkTruckId(truckId, token)
     fun checkStationId(token: String, stationId: String) = apiServices.checkMineStationId(stationId, token)
-    fun getToken() = preferences.getToken()
-    fun getName() = preferences.getName()
-    fun getRole() = preferences.getRole()
 }
