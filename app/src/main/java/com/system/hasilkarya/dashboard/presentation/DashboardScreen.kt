609c1dd326e0c3f55ea85d08c4505ee8efb49c05
Sendiko <sarangtawon897@gmail.com>
2024-06-09 09:05:59 +0700

    enc(add): update top app bar

--- app/src/main/java/com/system/hasilkarya/dashboard/presentation/DashboardScreen.kt
+++ app/src/main/java/com/system/hasilkarya/dashboard/presentation/DashboardScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,7 +26,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
@@ -49,10 +52,12 @@ import com.system.hasilkarya.core.navigation.MaterialScreen
 import com.system.hasilkarya.core.navigation.ProfileScreen
 import com.system.hasilkarya.core.navigation.StationScreen
 import com.system.hasilkarya.core.network.Status
+import com.system.hasilkarya.core.ui.components.LoadingIndicator
 import com.system.hasilkarya.core.ui.theme.poppinsFont
 import com.system.hasilkarya.dashboard.presentation.component.MenuCard
 import com.system.hasilkarya.station.presentation.component.StationLocation
 import com.system.hasilkarya.dashboard.presentation.component.InfoItemCard
+import java.time.LocalDateTime
 import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
@@ -63,7 +68,6 @@ fun DashboardScreen(
     onEvent: (DashboardScreenEvent) -> Unit,
     onNavigate: (destination: Any) -> Unit,
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val noStation = state.activeStation == null
 
     val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
@@ -84,12 +88,38 @@ fun DashboardScreen(
     )
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
+            LargeTopAppBar(
                 title = {
-                    Text(
-                        text = "Sistem Hasil Karya",
-                        fontFamily = poppinsFont
-                    )
+                    val time = LocalDateTime.now().hour
+                    val greeting = when (time) {
+                        in 5..11 -> {
+                            "Pagi"
+                        }
+                        in 12..15 -> {
+                            "Siang"
+                        }
+                        in 15..17 -> {
+                            "Sore"
+                        }
+                        else -> {
+                            "Malam"
+                        }
+                    }
+                    Column(
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    ) {
+                        Text(
+                            text = "Selamat $greeting,",
+                            fontFamily = poppinsFont,
+                            fontSize = 18.sp
+                        )
+                        Text(
+                            text = state.name,
+                            fontFamily = poppinsFont,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 },
                 actions = {
                     IconButton(onClick = { onNavigate(ProfileScreen) }) {
@@ -98,40 +128,13 @@ fun DashboardScreen(
                             contentDescription = "Settings",
                         )
                     }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                    scrolledContainerColor = MaterialTheme.colorScheme.background,
-                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                    actionIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant
-                ),
-                scrollBehavior = scrollBehavior
+                }
             )
         },
     ) { paddingValues ->
-        Column {
-            Column(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.surfaceVariant)
-                    .fillMaxWidth()
-                    .padding(
-                        top = paddingValues.calculateTopPadding(),
-                        start = 16.dp,
-                        end = 16.dp,
-                        bottom = 16.dp
-                    ),
-//                verticalAlignment = Alignment.Bottom
-            ) {
-                Text(
-                    text = "Selamat datang, ",
-                    fontFamily = poppinsFont,
-                )
-                Text(
-                    text = state.name,
-                    fontFamily = poppinsFont,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
+        Column(modifier = Modifier.padding(paddingValues)) {
+            AnimatedVisibility(visible = state.isConnecting || state.isUploading) {
+                LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
             }
             LazyColumn(
                 contentPadding = PaddingValues(
@@ -139,7 +142,6 @@ fun DashboardScreen(
                     end = 16.dp,
                 ),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
                 content = {
                     if (state.role == "checker") {
                         item {
@@ -156,6 +158,7 @@ fun DashboardScreen(
                                 onButtonClick = {
                                     onNavigate(StationScreen)
                                 },
+                                modifier = Modifier.padding(bottom = 8.dp)
                             )
                         }
                     }
@@ -181,7 +184,7 @@ fun DashboardScreen(
                     if (state.role == "gas-operator" || state.role == "admin") {
                         item {
                             Row(
-                                modifier = Modifier.fillMaxWidth()
+                                modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
                             ) {
                                 MenuCard(
                                     text = "Scan Transaksi BBM Truk",
@@ -203,25 +206,6 @@ fun DashboardScreen(
                                     modifier = Modifier.weight(1f)
                                 )
                             }
-                        }
-                    }
-                    item {
-                        AnimatedVisibility(
-                            visible = state.totalData != 0,
-                            enter = expandHorizontally(),
-                            exit = shrinkHorizontally()
-                        ) {
-                            InfoItemCard(
-                                modifier = Modifier
-                                    .fillMaxWidth(),
-                                info = "${state.totalData} data menunggu diupload.",
-                                icon = {
-                                    Icon(
-                                        imageVector = Icons.Default.Info,
-                                        contentDescription = "info"
-                                    )
-                                }
-                            )
                         }
                     }
                     if (noStation && state.role == "checker") {
